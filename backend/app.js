eda261b62384998f8805e498721629d1aefef74c
SolenPB <solene.paratre@gmail.com>
2023-09-05 22:19:14 +0200

    maj app

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use('/api/stuff', (req, res, next) => {
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/books', (req, res, next) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
