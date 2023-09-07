041707fa4ffccdcfca3b6f2c1527e37981d7ad4e
SolenPB <solene.paratre@gmail.com>
2023-09-07 13:46:17 +0200

    maj connexion mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://UserTest1:QYPYngmwOHVrSdSq@clusterbook.hmquoux.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true})
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 const app = express();
 
 app.use(express.json());
