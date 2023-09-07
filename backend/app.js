1a757a67bf4942a6cfa9630fa4d7630c95a6f26f
SolenPB <solene.paratre@gmail.com>
2023-09-07 18:25:33 +0200

    config routes

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 
+const bookRoutes = require('./routes/book');
+
 mongoose.connect('mongodb+srv://UserTest1:QYPYngmwOHVrSdSq@clusterbook.hmquoux.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true})
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const Book = require('./models/Book');
+
 
 const app = express();
 
@@ -21,38 +23,6 @@ app.use((req, res, next) => {
     next();
   });
 
-app.post('/api/books', (req, res, next) => {
-  delete req.body._id;
-  const book = new Book({
-    ...req.body
-  });
-  book.save()
-    .then(()=> res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/books/:id', (req, res, next) => {
-  Book.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ Message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/books/:id', (req,res,next) => {
-  Book.findOne({ _id: req.params.id})
-    .then(book => res.status(200).json(book))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/books', (req, res, next) => {
-    Book.find()
-      .then(books => res.status(200).json(books))
-      .catch(error => res.status(400).json({ error }));
-  });
-
-app.delete('/api/books/:id', (req, res, next) => {
-  Book.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/books', bookRoutes);
 
 module.exports = app;
