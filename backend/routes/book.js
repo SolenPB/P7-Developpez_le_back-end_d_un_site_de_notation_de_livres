3b2d2ec1a811f332c9e6be7a2538d9096446ff6d
SolenPB <solene.paratre@gmail.com>
2023-09-08 18:19:40 +0200

    add auth

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 
-router.put('/:id', bookCtrl.modifyBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
 
-router.delete('/:id', bookCtrl.deleteBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
   
-router.get('/:id', bookCtrl.getOneBook);
+router.get('/:id', auth, bookCtrl.getOneBook);
   
-router.get('/', bookCtrl.getAllBook);
+router.get('/', auth, bookCtrl.getAllBook);
   
 
 module.exports = router;
