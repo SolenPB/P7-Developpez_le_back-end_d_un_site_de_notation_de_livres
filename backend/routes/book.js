aa71dafd2a236d559ff0113c96c82a63f33e0884
SolenPB <solene.paratre@gmail.com>
2023-09-08 10:44:15 +0200

    add controllers

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,40 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-const Book = require('../models/Book');
+const bookCtrl = require('../controllers/book');
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const book = new Book({
-      ...req.body
-    });
-    book.save()
-      .then(()=> res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.post('/', bookCtrl.createBook);
+
+router.put('/:id', bookCtrl.modifyBook);
+
+router.delete('/api/books/:id', bookCtrl.deleteBook);
   
-router.put('/:id', (req, res, next) => {
-    Book.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-      .then(() => res.status(200).json({ Message: 'Objet modifié !' }))
-      .catch(error => res.status(400).json({ error }));
-  });
+router.get('/:id', bookCtrl.getOneBook);
   
-router.get('/:id', (req,res,next) => {
-    Book.findOne({ _id: req.params.id})
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  });
+router.get('/', bookCtrl.getAllBook);
   
-router.get('/', (req, res, next) => {
-      Book.find()
-        .then(books => res.status(200).json(books))
-        .catch(error => res.status(400).json({ error }));
-    });
-  
-router.delete('/api/books/:id', (req, res, next) => {
-    Book.deleteOne({ _id: req.params.id })
-      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
 
 module.exports = router;
