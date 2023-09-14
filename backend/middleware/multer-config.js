88e2bd8456a6b57ed1bf606336201f1c81dfc084
SolenPB <solene.paratre@gmail.com>
2023-09-14 16:12:33 +0200

    maj projet

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharpMulter = require('sharp-multer');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -7,14 +8,13 @@ const MIME_TYPES = {
 };
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images')
-    },
+    destination: (req, file, callback) => callback(null, 'images'),
     filename: (req, file, callback) => {
         const name = file.originalname.split(' ').join('_');
         const extension = MIME_TYPES[file.mimetype];
         callback(null, name + Date.now() + '.' + extension);
-    }
+    },
+
 });
 
 module.exports = multer({ storage }).single('image');
