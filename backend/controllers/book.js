6b7c14b0ff833114f65bbcddf2b5c4d16ae0e081
SolenPB <solene.paratre@gmail.com>
2023-09-16 18:37:59 +0200

    tentative sharp

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,16 +1,17 @@
 const Book = require('../models/Book');
 const fs = require('fs');
+const sharp = require('sharp');
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject._userId;
+
     const book = new Book({
       ...bookObject, 
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-
     book.save() 
       .then(() => { res.status(201).json({ message: 'Objet enregistré !'})})
       .catch(error => {res.status(400).json({ error })})
