c79cca3e0620a2afe50640a0b6ebb80272a2a146
SolenPB <solene.paratre@gmail.com>
2023-09-17 16:05:58 +0200

    ajout rate limit

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,18 +5,19 @@ const multer = require('../middleware/multer-config');
 const rateLimit = require('express-rate-limit');
 
 const apiLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
     limit: 15,
 })
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, bookCtrl.createBook);
+router.post('/', auth, multer, apiLimiter, bookCtrl.createBook);
 
-router.post('/:id/rating', auth, bookCtrl.ratingBook);
+router.post('/:id/rating', auth, apiLimiter, bookCtrl.ratingBook);
 
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, apiLimiter, bookCtrl.modifyBook);
 
-router.delete('/:id', auth, bookCtrl.deleteBook);
+router.delete('/:id', auth, apiLimiter, bookCtrl.deleteBook);
 
 router.get('/bestrating', bookCtrl.bestRatedBooks);
   
