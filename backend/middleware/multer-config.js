77ca61ff022984348cbed877db51689eeac7b9d1
SolenPB <solene.paratre@gmail.com>
2023-09-18 21:37:20 +0200

    maj resized image

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,18 +1,11 @@
 const multer = require('multer');
-const sharp = require('sharp');
+const sharpMulter = require('sharp-multer');
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpeg',
-    'image/png': 'png'
-};
-
-const storage = multer.diskStorage({
+const storage = sharpMulter({
     destination: (req, file, callback) => callback(null, 'images'),
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+    imageOptions:{
+        fileFormat: "webp",
+        resize: { width: 206, height: 260 },
     }
 });
 
