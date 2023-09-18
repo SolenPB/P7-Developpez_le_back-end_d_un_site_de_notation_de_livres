737cb99f35abeab6fc9945ecf9ed1f3575b0c3d4
SolenPB <solene.paratre@gmail.com>
2023-09-18 18:23:38 +0200

    maj rate limit

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,22 +2,16 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
-const rateLimit = require('express-rate-limit');
-
-const apiLimiter = rateLimit({
-    windowMs: 1 * 60 * 1000,
-    limit: 15,
-})
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, apiLimiter, bookCtrl.createBook);
+router.post('/', auth, multer, bookCtrl.createBook);
 
-router.post('/:id/rating', auth, apiLimiter, bookCtrl.ratingBook);
+router.post('/:id/rating', auth, bookCtrl.ratingBook);
 
-router.put('/:id', auth, multer, apiLimiter, bookCtrl.modifyBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
 
-router.delete('/:id', auth, apiLimiter, bookCtrl.deleteBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 router.get('/bestrating', bookCtrl.bestRatedBooks);
   
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const rateLimit = require('express-rate-limit');
+
+const apiLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    limit: 15,
+})
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', apiLimiter, userCtrl.login);
 
 module.exports = router;
