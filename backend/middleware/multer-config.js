ae332e19f9b218c6d43c2a4ea8300f4127787f98
SolenPB <solene.paratre@gmail.com>
2023-09-26 12:19:40 +0200

    ajout mime types

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 const sharpMulter = require('sharp-multer');
 
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+  };
+  
 const storage = sharpMulter({
     destination: (req, file, callback) => callback(null, 'images'),
     imageOptions:{
