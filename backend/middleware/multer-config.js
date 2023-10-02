cf72f1b93ef70b9654744d1646181900afec087e
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-10-02 11:54:24 +0200

    modify project

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,18 +1,25 @@
 const multer = require('multer');
 const sharpMulter = require('sharp-multer');
+const { options } = require('../app');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpeg',
     'image/png': 'png'
   };
-
+const newFilename = (originalfilename, imageOptions) => {
+    const newName = 
+        originalfilename.split(".").slice(0, -1).join("_") + Date.now() + "." +
+        imageOptions.fileFormat;
+    return newName;
+}
 const storage = sharpMulter({
     destination: (req, file, callback) => callback(null, 'images'),
     imageOptions:{
         fileFormat: "webp",
         resize: { width: 206, height: 260 },
-    }
+    },
+    filename: newFilename,
 });
 
 
