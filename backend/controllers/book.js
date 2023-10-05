2d9ddcba7d694c09bcb681367500ac36b770b792
SolenPB <126073154+SolenPB@users.noreply.github.com>
2023-10-05 21:29:38 +0200

    maj project

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,6 +1,17 @@
 const Book = require('../models/Book');
 const fs = require('fs');
 
+exports.getAllBook = (req, res, next) => {
+  Book.find()
+    .then(books => res.status(200).json(books))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneBook = (req,res,next) => {
+  Book.findOne({ _id: req.params.id})
+    .then(book => res.status(200).json(book))
+    .catch(error => res.status(404).json({ error }));
+};
 
 exports.createBook = (req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
@@ -103,16 +114,4 @@ exports.deleteBook =  (req, res, next) => {
       .catch( error => {
         res.status(500).json({ error });
       })
-  };
-
-exports.getOneBook = (req,res,next) => {
-    Book.findOne({ _id: req.params.id})
-      .then(book => res.status(200).json(book))
-      .catch(error => res.status(404).json({ error }));
-  };
-
-exports.getAllBook = (req, res, next) => {
-    Book.find()
-      .then(books => res.status(200).json(books))
-      .catch(error => res.status(400).json({ error }));
   };
